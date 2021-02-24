f06b6098e0e059de044f2384de6d0454e7a137cf
Florian Thomas <flthomas@students.uni-mainz.de>
2021-02-24 15:38:51 +0100

    Add a test for running on grace

---
+++ test/testLocustCluster.py
@@ -0,0 +1,23 @@
+
+"""
+
+Author: F. Thomas
+Date: February 22, 2021
+
+"""
+
+from hercules.simulation import SimConfig, KassLocustP3Cluster
+from pathlib import Path
+
+moduleDir = Path(__file__).parent.absolute()
+
+#just an example
+config = SimConfig(nChannels=2, seedLocust=1, vRange=7.0,
+                    eggFilename='someFileName.egg', seedKass =12534, xMin=-0.1e-5, 
+                    xMax=0.1e-5, tMax=0.5e-6,
+                    geometry='FreeSpaceGeometry_V00_00_10.xml')
+
+
+sim = KassLocustP3Cluster(str(moduleDir) + '/workingDir')
+sim(config, 'someDirName')
+
