a0e62edfb9aea5bd84e6b15277fa04046fe73a7e
Florian Thomas <flthomas@students.uni-mainz.de>
2021-03-11 13:27:48 +0100

    Modify locust test
    The test now runs a batch of parallel jobs

--- test/testlocust.py
+++ test/testlocust.py
@@ -14,16 +14,16 @@ module_dir = Path(__file__).parent.absolute()
 
 #just an example
 config_list = []
+sim = he.KassLocustP3(str(module_dir) + '/workingDir')
 
-r_vals = np.linspace(0.0,0.03,10)
+simulations = 20
+r_vals = np.linspace(0.0, 0.03, simulations)
 
-for r in r_vals:
-    config = he.SimConfig('someDirName_'+str(r), n_channels=2, seed_locust=1, 
+for i, r in enumerate(r_vals):
+    config = he.SimConfig('someDirName_{0:1.3f}'.format(r), n_channels=2, seed_locust=1, 
                             v_range=7.0, egg_filename='someFileName.egg', 
                             x_min=r, x_max=r, t_max=0.5e-6,
                             geometry='FreeSpaceGeometry_V00_00_10.xml')
     config_list.append(config)
 
-
-sim = he.KassLocustP3(str(module_dir) + '/workingDir')
 sim(config_list)
