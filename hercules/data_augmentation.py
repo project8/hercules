407dfd9e01fc620e57b7c679aa05961b26cc778a
Wouter-VDP <woutervdp@gmail.com>
2021-06-30 14:10:07 -0400

    updating the white gaussian noise to complex data

--- hercules/data_augmentation.py
+++ hercules/data_augmentation.py
@@ -22,15 +22,16 @@ def whitegaussiannoise(data, param_dict):
     White Gaussian Noise added to the voltage time series
     This implementation is fully uncorrelated over time/channels.
     Requires a temperature and bandwidth
-    TO DO: Check amplitude calculation?
+    https://en.wikipedia.org/wiki/Johnson%E2%80%93Nyquist_noise
+    50 Ohm matched transmission line
     """
-    noise_amp = np.sqrt(constants.k*param_dict['noise_temperature']*param_dict['bandwidth'])
-    return data+np.random.randn(*data.shape)*noise_amp
+    noise_amp = np.sqrt(50*constants.k*param_dict['noise_temperature']*param_dict['bandwidth'])
+    noise = np.random.multivariate_normal([0,0], np.eye(2) * noise_amp / 2, data.shape).view(np.complex128)
+    return data+noise
 
 def experimental_noise(data, param_dict):
     """
     None white noise, meaning this is frequency dependent but still uncorrelated for different parameters
     Requires a noise PSD and normalisation.
-    To Be implemented
     """
     return 0
