1cce8fc2137a07bf06ef67b4fdb78f8d90e98d3a
Florian Thomas <flthomas@students.uni-mainz.de>
2021-07-19 19:29:36 +0200

    Hotfix for docker container on desktop
    The fix makes hercules select the version of the docker container that
    is provided in the config.ini. Before it was possible that the wrong
    version was uesd if multiple versions were present

--- hercules/configuration.py
+++ hercules/configuration.py
@@ -16,11 +16,11 @@ _CONFIGDIR = _MODULEDIR/'settings'/'config.ini'
 
 
 class Configuration:
-    
+
     def __init__(self):
-        
+
         config = configparser.ConfigParser()
-        
+
         try:
             with open(_CONFIGDIR) as f:
                 config.read_file(f)
@@ -29,37 +29,38 @@ class Configuration:
                     + 'Copy hercules/settings/config.default.ini '
                     + 'to hercules/settings/config.ini, adjust to your needs '
                     + 'and run pip install again!')
-        
+
         self._handle_env(config)
-        
+
         self._locust_version = config['PACKAGE']['LOCUSTVERSION']
         self._locust_path = config['PACKAGE']['LOCUSTPATH']
         self._p8compute_version = config['PACKAGE']['P8COMPUTEVERSION']
         self._p8compute_path = config['PACKAGE']['P8COMPUTEPATH']
         self._desktop_parallel_jobs = config['USER']['DESKTOP_PARALLEL_JOBS']
-        
+
         self._partition = config['GRACE']['JOB_PARTITION']
         self._job_timelimit = config['GRACE']['JOB_TIMELIMIT']
         self._job_memory = config['GRACE']['JOB_MEMORY']
         self._job_limit = config['GRACE']['JOB_LIMIT']
-                              
+
     def _handle_env(self, config):
-        
+
         self._env = config['USER']['ENVIRONMENT']
-        
+
         if self._env == 'desktop':
-            self._container = config['DESKTOP']['CONTAINER']
+            self._container = (config['DESKTOP']['CONTAINER']
+                                + ':' + config['PACKAGE']['P8COMPUTEVERSION'])
         elif self._env == 'grace':
             self._container = config['GRACE']['CONTAINER']
         else:
-            raise ValueError((self._env 
+            raise ValueError((self._env
                               + ' is not a valid environment setting.'
                               + ' Check settings/config.ini'))
-    
+
     @property
     def locust_version(self):
         return self._locust_version
-        
+
     @property
     def locust_path(self):
         return self._locust_path
@@ -67,35 +68,35 @@ class Configuration:
     @property
     def p8compute_version(self):
         return self._p8compute_version
-        
+
     @property
     def p8compute_path(self):
         return self._p8compute_path
-        
+
     @property
     def env(self):
         return self._env
-        
+
     @property
     def container(self):
         return self._container
-        
+
     @property
     def desktop_parallel_jobs(self):
         return self._desktop_parallel_jobs
-        
+
     @property
     def partition(self):
         return self._partition
-        
+
     @property
     def job_timelimit(self):
         return self._job_timelimit
-        
+
     @property
     def job_memory(self):
         return self._job_memory
-        
+
     @property
     def job_limit(self):
         return self._job_limit
