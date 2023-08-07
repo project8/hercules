99df0b1e0c5f3e33472451ae65f823739f0303d4
Florian Thomas <fthomas@uni-mainz.de>
2023-08-07 17:37:52 +0200

    Update old example scripts

--- examples/locustP3_example.py
+++ examples/locustP3_example.py
@@ -6,24 +6,24 @@ Date: February 22, 2021
 
 """
 
-import hercules as he
+from hercules import SimConfig, KassLocustP3, ConfigList
 from pathlib import Path
 import numpy as np
 
 module_dir = Path(__file__).parent.absolute()
 
 #just an example
-config_list = []
-sim = he.KassLocustP3(str(module_dir) + '/workingDir')
+sim = KassLocustP3(str(module_dir) + '/workingDirP3', use_kass=True, use_locust=True)
+configlist = ConfigList()
 
-simulations = 20
+simulations = 5
 r_vals = np.linspace(0.0, 0.03, simulations)
 
 for i, r in enumerate(r_vals):
-    config = he.SimConfig('someDirName_{0:1.3f}'.format(r), n_channels=2, seed_locust=1, 
+    config = SimConfig(n_channels=2, seed_locust=1, 
                             v_range=7.0, egg_filename='someFileName.egg', 
                             x_min=r, x_max=r, t_max=0.5e-6,
                             geometry='FreeSpaceGeometry_V00_00_10.xml')
-    config_list.append(config)
+    configlist.add_config(config)
 
-sim(config_list)
+sim(configlist)
