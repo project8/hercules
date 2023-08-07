b5a1be14066aad2b52f158906c6718be22687660
Florian Thomas <fthomas@uni-mainz.de>
2023-08-07 14:30:40 +0200

    Add unittest for the ConfigList class

--- test/test_config.py
+++ test/test_config.py
@@ -135,15 +135,17 @@ class SimpleSimConfigTest(unittest.TestCase):
         self.assertTrue(self.config.get_meta_data()==meta_data)
 
 
-
-"""
 class ConfigListTest(unittest.TestCase):
         
     def setUp(self) -> None:
             
-        self.configlist = ConfigList(acquisition_rate=1., info='hello', trap='nonsense')
-
         n_channels = 3
+        self.configlist = ConfigList(acquisition_rate=1., info='hello', trap='nonsense')
+        self.configlist_simple = ConfigList(acquisition_rate=1., info='hello simple', trap='nonsense', n_channels=n_channels)
+
+        self.configlist_l = []
+        self.configlist_simple_l = []
+
         r_range = np.linspace(0.002, 0.008, 8)
         theta_range = np.linspace(89.7, 90.0, 30)
 
@@ -154,9 +156,8 @@ class ConfigListTest(unittest.TestCase):
                 for r in r_range:
                     x = r * np.cos(r_phi)
                     y = r * np.sin(r_phi)
-                    r_phi_deg = np.rad2deg(r_phi)
 
-                    config = SimConfig(
+                    sim_config = SimConfig(
                                         n_channels=n_channels,
                                         seed_locust=42,
                                         seed_kass=43,
@@ -173,96 +174,103 @@ class ConfigListTest(unittest.TestCase):
                                         v_range=3.0e-7,
                                         geometry='FreeSpaceGeometry_V00_00_10.xml')
                     
-                    self.clist.add_config(config)
+                    self.configlist_l.append(sim_config)
 
-                # json_file = FILE_DIR/'SimConfig.json'
-                # locust_file = FILE_DIR/'Locust.json'
-                    # Note kass file name has to be the following for dict comparison
-                # kass_file = FILE_DIR/'LocustKassElectrons.xml'
+                    simple_config = SimpleSimConfig(r=r, phi=r_phi, z=0., energy=0., pitch=90.)
+                    self.configlist_simple_l.append(simple_config)
 
-                # config.to_json(json_file)
-
-                    #config2 = he.SimConfig.from_json(json_file)
-                    #config2.make_config_file(locust_file, kass_file)
-                    #self.assertDictEqual(config.to_dict(), config2.to_dict())
-
-                    #config_list.append(config)
-
-    def test_meta_data(self) -> None:
-    
-        expected = {'acquisition_rate': 1.0, 'info': 'hello', 'trap': '[config_path]/Trap/FreeSpaceGeometry_V00_00_10.xml', 
-                    'transfer-function': '/tmp/Phase3/TransferFunctions/FiveSlotTF.txt', 'n-channels': 3, 'lo-frequency': 25878100000.0}
+    def test_add_config(self):
         
-        self.assertTrue(self.clist.get_meta_data()==expected)
+        self.configlist.add_config(self.configlist_l[0])
+        self.configlist.add_config(self.configlist_l[1])
+
+        #adding wrong type
+        with self.assertRaises(TypeError) as cm:
+            self.configlist.add_config(self.configlist_simple_l[0])
+
+        self.configlist_simple.add_config(self.configlist_simple_l[0])
+        self.configlist_simple.add_config(self.configlist_simple_l[1])
+
+        #adding non matching config data
+        with self.assertRaises(RuntimeError) as cm:
+            self.configlist_simple.add_config(SimpleSimConfig(x='foo'))
+
+    def test_type(self):
+
+        self.configlist.add_config(self.configlist_l[0])
+
+        self.configlist_simple.add_config(self.configlist_simple_l[0])
+
+        self.assertEqual(self.configlist.get_list_type(), SimConfig)
+        self.assertEqual(self.configlist_simple.get_list_type(), SimpleSimConfig)
+
+
+    def add_all(self):
+        for config in self.configlist_l:
+            self.configlist.add_config(config)
+
+        for config in self.configlist_simple_l:
+            self.configlist_simple.add_config(config)
+
+    def test_add_all(self):
+
+        self.add_all()
+
+        self.assertEqual(len(self.configlist.get_internal_list()), len(self.configlist_l))
+        self.assertEqual(len(self.configlist_simple.get_internal_list()), len(self.configlist_simple_l))
+
+    def test_meta_data(self):
+
+        self.add_all()
+
+        expected = {'acquisition_rate': 1.0,
+                    'info': 'hello',
+                    'trap': '[config_path]/Trap/FreeSpaceGeometry_V00_00_10.xml',
+                    'transfer-function': '/tmp/Phase3/TransferFunctions/FiveSlotTF.txt',
+                    'n-channels': 3,
+                    'lo-frequency': 25878100000.0}
         
-        for config in self.clist.get_internal_list():
-             self.assertTrue(config.get_meta_data()==expected)
+        self.assertEqual(expected, self.configlist.get_meta_data())
 
-    def test_config_data(self) ->None:
+        for config in self.configlist.get_internal_list():
+            self.assertEqual(expected, config.get_meta_data())
 
+        expected = {'acquisition_rate': 1.0,
+                    'info': 'hello simple',
+                    'trap': 'nonsense',
+                    'n_channels': 3}
         
+        self.assertEqual(expected, self.configlist_simple.get_meta_data())
 
-    def test_generate_configs(self) -> None:
+        for config in self.configlist_simple.get_internal_list():
+            self.assertEqual(expected, config.get_meta_data())
 
-        clist = ConfigList(acquisition_rate=1., info='hello')
-        # Generates a list of config
-        n_channels = 3
-        r_range = np.linspace(0.002, 0.008, 8)
-        theta_range = np.linspace(89.7, 90.0, 30)
+    def test_config_data(self):
 
-        r_phi_range = np.linspace(0, 2 * np.pi / n_channels, 1)
+        self.add_all()
+        
+        expected = ['energy', 'phi', 'pitch', 'r', 'z']
 
-        config_list = []
+        self.assertEqual(sorted(list(self.configlist.get_config_data_keys())), expected)
+        self.assertEqual(sorted(list(self.configlist_simple.get_config_data_keys())), expected)
 
-        for theta in theta_range:
-            for r_phi in r_phi_range:
-                for r in r_range:
-                    x = r * np.cos(r_phi)
-                    y = r * np.sin(r_phi)
-                    r_phi_deg = np.rad2deg(r_phi)
-                    name = "Sim_theta_{:.4f}_R_{:.4f}_phi_{:.4f}".format(
-                        theta, r, r_phi_deg)
-                    config = he.SimConfig(
-                                        n_channels=n_channels,
-                                        seed_locust=42,
-                                        seed_kass=43,
-                                        egg_filename="simulation.egg",
-                                        x_min=x,
-                                        x_max=x,
-                                        y_min=y,
-                                        y_max=y,
-                                        z_min=0,
-                                        z_max=0,
-                                        theta_min=theta,
-                                        theta_max=theta,
-                                        t_max=5e-6,
-                                        v_range=3.0e-7,
-                                        geometry='FreeSpaceGeometry_V00_00_10.xml')
+        for config in self.configlist.get_internal_list():
+            config_data_keys = config.get_config_data().keys()
+            self.assertEqual(sorted(list(config_data_keys)), expected)
 
-                    json_file = FILE_DIR/'SimConfig.json'
-                    locust_file = FILE_DIR/'Locust.json'
-                    # Note kass file name has to be the following for dict comparison
-                    kass_file = FILE_DIR/'LocustKassElectrons.xml'
+        for config in self.configlist_simple.get_internal_list():
+            config_data_keys = config.get_config_data().keys()
+            self.assertEqual(sorted(list(config_data_keys)), expected)
 
-                    config.to_json(json_file)
+    def test_names(self):
 
-                    config2 = he.SimConfig.from_json(json_file)
-                    config2.make_config_file(locust_file, kass_file)
-                    self.assertDictEqual(config.to_dict(), config2.to_dict())
+        self.add_all()
 
-                    config_list.append(config)
+        for i, config in enumerate(self.configlist.get_internal_list()):
+            self.assertEqual(config.sim_name,f'run{i}')
 
-    def tearDown(self) -> None:
-        # Clean up
-        files_json = FILE_DIR.glob('*.json')
-        files_xml = FILE_DIR.glob('*.xml')
-        filtered_files = sorted(files_json) + sorted(files_xml)
-
-        for f in filtered_files:
-            # Note f is a Path object
-            # Deletes the file
-            f.unlink()
-"""
+        for i, config in enumerate(self.configlist_simple.get_internal_list()):
+            self.assertEqual(config.sim_name,f'run{i}')
 
 if __name__ == '__main__':
     unittest.main()
