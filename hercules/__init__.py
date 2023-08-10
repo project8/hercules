a3414b17549ef38b10d7d22d6666240cbd6d57d2
Florian Thomas <fthomas@uni-mainz.de>
2023-08-10 13:40:00 +0200

    Add hexbug version variable

--- hercules/__init__.py
+++ hercules/__init__.py
@@ -6,10 +6,14 @@ Date: February 17, 2021
 
 """
 
+
 from .simulation import KassLocustP3
 from .simconfig import SimConfig, SimpleSimConfig, ConfigList
 from .eggreader import LocustP3File
 from .dataset import Dataset
 
 from . import _version
+from . import constants
+
 __version__ = _version.get_versions()['version']
+__hexbug_version__ = constants.HEXBUG_COMMIT
